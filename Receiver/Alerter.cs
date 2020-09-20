4fdcdbc9fcff21d13657619fdb8bb38e2d2c72e0
shrutibnehe <56735035+shrutibnehe@users.noreply.github.com>
2020-09-20 15:42:46 +0530

    Update Alerter.cs

--- Receiver/Alerter.cs
+++ Receiver/Alerter.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Receiver
 {
+public abstract class Alerter
+    {
+        public abstract string SendAlert(string parameterName, string message, string parameterValue);
+    }
 
+    public class ConsoleAlerter : Alerter
+    {
+        public override string SendAlert(string parameterName, string message, string parameterValue)
+        {
+            Console.WriteLine($"{parameterName} {message} {parameterValue}");
+            return $"{parameterName} {message} {parameterValue}";
+        }
+    }
        
     
 }

f9bbf6c224f51ca538efca8335c438f7f03c8e30
lavandesh <31106149+lavandesh@users.noreply.github.com>
2020-09-20 15:14:20 +0530

    Update FileReader.cs

--- Sender/FileReader.cs
+++ Sender/FileReader.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -7,7 +7,48 @@ namespace Sender
 {
     public class FileReader
     {
-       
+        public bool CheckHeader(String line)
+        {
+
+            return (line.Contains("Temperature") && line.Contains("Humidity"));
+        }
+
+        public List<string> CheckFileExists(string path)
+        {
+            if (File.Exists(path))
+            {
+                var dataList = ReadCsvFromFile(path);
+                return dataList;
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+                return null;
+            }
+        }
+        public List<string> ReadCsvFromFile(string path)
+        {
+            var dataList = new List<string>();
+            using (TextReader sr = new StreamReader(path))
+            {
+                var line = sr.ReadLine();
+
+                if (!CheckHeader(line))
+                {
+                    dataList.Add(line);
+                }
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //Console.WriteLine(line);
+                    dataList.Add(line);
+
+                }
+            }
+
+            return dataList;
+        }
+
     }
 
 

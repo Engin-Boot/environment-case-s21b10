e8f6925e1d3a95e87302896b202d841b4b45a498
shrutibnehe <56735035+shrutibnehe@users.noreply.github.com>
2020-09-21 22:12:05 +0530

    Update Program.cs

--- Sender/Program.cs
+++ Sender/Program.cs
@@ -1,8 +1,3 @@
-﻿using System;
-
-using System.Threading;
-
-
 namespace Sender
 {
     class Program
@@ -11,23 +6,8 @@ namespace Sender
         {
            // string path = @"D:\C#\EnvironmentCaseStudy\Sender\bin\Debug\TemperatureAndHumidityData.csv";
             string path = @"D:\a\environment-case-s21b10\environment-case-s21b10\TemperatureAndHumidityData.csv";
-            FileReader readFile = new FileReader();
             DataSenderViaConsole sendData=new DataSenderViaConsole();
-
-            
-            var data = readFile.CheckFileExists(path);
-            if (data.Count == 0)
-            {
-                Console.WriteLine("CSV FILE EMPTY");
-            }
-            else
-            {
-                foreach (string dataRead in data)
-                {
-                    sendData.SendDataToReceiverViaConsole(dataRead);
-                    Thread.Sleep(1000);
-                }
-            }
+            sendData.SendDataToReceiver(path);
         }
     }
 }
